73225b0956a7d3462e57de8d8c4f52d9f14f6555
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-05-23 14:51:40 -0400

    Added Pupgrades

--- game.js
+++ game.js
@@ -6,3 +6,11 @@ let points = 0
                 pointsTracker.innerText = points + 'Pet Points'
     
             }
+
+function pupgrades () {
+
+function gameLoop(){
+    points=points + (0.2 * pets)
+    pointsTracker.innerText = points.toFixed(2) + 'Pet Points'
+}
+}
