95b78bc4ad12e5875a2caa7dd387cfaf39ac62b7
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-05-28 14:53:37 -0400

    Added clicker functions

--- game.js
+++ game.js
@@ -1,16 +1,33 @@
 let points = 0
-            let pointsTracker = document.getElementsById("pointsTracker")
+let sbs = 0
+let pointsTracker = document.getElementById("pointsTracker")
 
-            function increasePoints() {
-                points = points + 1
-                pointsTracker.innerText = points + 'Pet Points'
-    
-            }
+function increasePoints() {
+    points = points + 1
+    console.log(points)
+    pointsTracker.innerText = points.toFixed(2)
 
-function pupgrades () {
-
-function gameLoop(){
-    points=points + (0.2 * pets)
-    pointsTracker.innerText = points.toFixed(2) + 'Pet Points'
 }
+
+function smallBoost() {
+    points = points - 10
+    sbs = sbs + 1
+    console.log(points)
+    pointsTracker.innerText = points.toFixed(2)
+
 }
+
+function mediumBoost() {
+    points = points - 20
+}
+
+function gameLoop() {
+    points = points + (0.2 * sbs)
+    pointsTracker.innerText = points.toFixed(2)
+}
+
+function petPoints() {
+}
+
+setInterval(gameLoop, 1000)
+
