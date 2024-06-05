03aff94ec9ea31e76f48104f36a0f3bb3bf1020b
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-06-05 14:51:59 -0400

    fixed buying upgrades issue and added sound effect

--- game.js
+++ game.js
@@ -9,22 +9,26 @@ let bd = 0
 let gs = 0
 let dm = 0
 let pointsTracker = document.getElementById("pointsTracker")
-let pupGcost = document.getElementById("pupGcost")
+let sbsCost = document.getElementById("sbsCost")
+let mbsCost = document.getElementById("mbsCost")
+let scCost = document.getElementById("scCost")
+let gcCost = document.getElementById("gcCost")
+let grCost = document.getElementById("grCost")
+let dlCost = document.getElementById("dlCost")
+let bdCost = document.getElementById("bdCost")
+let gsCost = document.getElementById("gsCost")
+let dmCost = document.getElementById("dmCost")
+
 
 
 function buyPupG() {
-    let cost = 10 + sbs ** 1.5
-    if (points >= cost) {
+    let cost = (10 + sbs ** 1.5).toFixed(0)
+    if (points.toFixed(0)>= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
-
+        pointsTracker.innerText = points.toFixed(0)
         sbs = sbs + 1
-
         sbsAmount.innerText = sbs + ' Small Boosts owned'
-        sbsCost.innerText = (10 + (sbs ** 1.5)).toFixed(0) + " Points"
-        pupGcost.innerText = 10 + sbs ** 1.5
-
-        cost = 10 + sbs ** 1.5
+        sbsCost.innerText = (10 + sbs ** 1.5).toFixed(0) + " Points"
     }
 
     else {
@@ -33,61 +37,50 @@ function buyPupG() {
     }
 }
 
-function buyMbs() {
-    let cost = 20 + mbs ** 1.5
-    if (points >= cost) {
+function buyMed() {
+    let cost = 20 + mbs ** 1.75
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
-
+        pointsTracker.innerText = points.toFixed(0)
         mbs = mbs + 1
-
         mbsAmount.innerText = mbs + ' Medium Boots owned'
-        mbsCost.innerText = (20 + (mbs ** 1.5)).toFixed(0) + " Points"
-        buyMbs.innerText = 20 + mbs ** 1.5
-
-        cost = 20 + mbs ** 1.5
+        mbsCost.innerText = (20 + mbs ** 1.75).toFixed(0) + " Points"
+        buyMbs.innerText = 20 + mbs ** 1.75
     }
 
-    else {
+    else{
         alert("not enough points")
 
     }
 }
 
-function buySuper() {
-    let cost = 30 + sc ** 1.5
-    if (points >= cost) {
+function buySup() {
+    let cost = 30 + sc ** 2
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
-
+        pointsTracker.innerText = points.toFixed(0)
         sc = sc + 1
-
         scAmount.innerText = sc + ' Large Boost owned'
-        scCost.innerText = (30 + (sc ** 1.5)).toFixed(0) + " Points"
-        buySuper.innerText = 30 + sc ** 1.5
-
-        cost = 30 + sc ** 1.5
+        scCost.innerText = (30 + sc ** 2).toFixed(0) + " Points"
+        buySuper.innerText = 30 + sc ** 2
     }
-
     else {
         alert("not enough points")
 
     }
 }
 
-function buyChanger() {
-    let cost = 40 + gc ** 1.5
-    if (points >= cost) {
+function buyChange() {
+    let cost = 40 + gc ** 2.25
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         gc = gc + 1
 
         gcAmount.innerText = gc + ' XL Boost owned'
-        gcCost.innerText = (40 + (gc ** 1.5)).toFixed(0) + " Points"
-        buyChanger.innerText = 40 + gc ** 1.5
-
-        cost = 40 + gc ** 1.5
+        gcCost.innerText = (40 + gc ** 2.25).toFixed(0) + " Points"
+        buyChanger.innerText = 40 + gc ** 2.25
     }
 
     else {
@@ -96,20 +89,18 @@ function buyChanger() {
     }
 }
 
-function goldenRet() {
-    let cost = 500 + gr ** 1.5
+function goldenR() {
+    let cost = 500 + gr ** 2.5
     console.log(points >= cost)
-    if (points >= cost) {
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         gr = gr + 1
 
         grAmount.innerText = gr + ' Super Chargers owned'
-        grCost.innerText = (500 + (gr ** 1.5)).toFixed(0) + " Points"
-        goldenRet.innerText = 500 + gr ** 1.5
-
-        cost = 500 + gr ** 1.5
+        grCost.innerText = (500 + gr ** 2.5).toFixed(0) + " Points"
+        goldenRet.innerText = 500 + gr ** 2.5
     }
 
     else {
@@ -118,40 +109,36 @@ function goldenRet() {
     }
 }
 
-function dalmatian() {
-    let cost = 1000 + dl ** 1.5
-    if (points >= cost) {
+function dalmat() {
+    let cost = 1000 + dl ** 2.75
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         dl = dl + 1
 
         dlAmount.innerText = dl + ' Mega Chargers owned'
-        dlCost.innerText = (1000 + (dl ** 1.5)).toFixed(0) + " Points"
-        dalmatian.innerText = 1000 + dl ** 1.5
-
-        cost = 1000 + dl ** 1.5
+        dlCost.innerText = (1000 + dl ** 2.75).toFixed(0) + " Points"
+        dalmatian.innerText = 1000 + dl ** 2.75
     }
-
     else {
         alert("not enough points")
 
     }
 }
 
-function boxerDog() {
-    let cost = 5000 + bd ** 1.5
-    if (points >= cost) {
+function boxerBuy() {
+    let cost = 5000 + bd ** 3
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         bd = bd + 1
 
         bdAmount.innerText = bd + ' Ultra Chargers owned'
-        bdCost.innerText = (5000 + (bd ** 1.5)).toFixed(0) + " Points"
-        boxerDog.innerText = 5000 + bd ** 1.5
+        bdCost.innerText = (5000 + bd ** 3).toFixed(0) + " Points"
+        boxerDog.innerText = 5000 + bd ** 3
 
-        cost = 5000 + bd ** 1.5
     }
 
     else {
@@ -160,19 +147,17 @@ function boxerDog() {
     }
 }
 
-function germanShep() {
-    let cost = 10000 + gs ** 1.5
-    if (points >= cost) {
+function germanbuy() {
+    let cost = 10000 + gs ** 3.25
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         gs = gs + 1
 
         gsAmount.innerText = gs + ' Ultimate Chargers owned'
-        gsCost.innerText = (10000 + (gs ** 1.5)).toFixed(0) + " Points"
-        germanShep.innerText = 10000 + gs ** 1.5
-
-        cost = 10000 + gs ** 1.5
+        gsCost.innerText = (10000 + gs ** 3.25).toFixed(0) + " Points"
+        germanShep.innerText = 10000 + gs ** 3.25
     }
 
     else {
@@ -181,19 +166,18 @@ function germanShep() {
     }
 }
 
-function doberMan() {
-    let cost = 50000 + dm ** 1.5
-    if (points >= cost) {
+function doberManBuy() {
+    let cost = 50000 + dm ** 5
+    if (points.toFixed >= cost) {
         points = points - cost
-        pointsTracker.innerText = points.toFixed(2)
+        pointsTracker.innerText = points.toFixed(0)
 
         dm = dm + 1
 
         dmAmount.innerText = dm + ' Game Changers owned'
-        dmCost.innerText = (5000 + (dm ** 1.5)).toFixed(0) + " Points"
-        doberMan.innerText = 50000 + dm ** 1.5
-
-        cost = 50000 + dm ** 1.5
+        dmCost.innerText = (5000 + dm ** 5).toFixed(0) + " Points"
+        doberMan.innerText = 50000 + dm ** 5
+        doberBark.play()
     }
 
     else {
@@ -201,10 +185,6 @@ function doberMan() {
 
     }
 }
-
-
-
-
 
 function increasePoints() {
     points = points + 1
@@ -213,77 +193,45 @@ function increasePoints() {
 
 }
 
-function smallBoost() {
-    points = points - 10
-    sbs = sbs + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function mediumBoost() {
-    points = points - 20
-    mbs = mbs + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function superCharge() {
-    points = points - 30
-    sc = sc + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function gameChanger() {
-    points = points - 40
-    gc = gc + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function dalmatian() {
-    points = points - 1000
-    dl = dl + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function boxerDog() {
-    points = points - 5000
-    bd = bd + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function germanShep() {
-    points = points - 10000
-    gs = gs + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
-function doberMan() {
-    points = points - 50000
-    dm = dm + 1
-    console.log(points)
-    pointsTracker.innerText = points.toFixed(2)
-}
-
 function gameLoop() {
     points = points + (0.2 * sbs)
     points = points + (0.4 * mbs)
     points = points + (0.6 * sc)
     points = points + (1.0 * gc)
     points = points + (5.0 * gr)
-    points = points + (15. * mbs)
-    points = points + (30. * sc)
-    points = points + (50.0 * gc)
-    points = points + (100.0 * gc)
+    points = points + (15. * dl)
+    points = points + (30. * bd)
+    points = points + (50.0 * gs)
+    points = points + (100.0 * dm)
     pointsTracker.innerText = points.toFixed(2)
-    if (points < 10) {
-        smallBoostBtn.disabled = true
+    if (points < 500) {
+        goldenBtn.disabled = true
     } else {
-        smallBoostBtn.disabled = false
+        goldenBtn.disabled = false
+    }
+
+    if (points < 1000) {
+        dalmBtn.disabled = true
+    } else {
+        dalmBtn.disabled = false
+    }
+
+    if (points < 5000) {
+        boxerBtn.disabled = true
+    } else {
+        boxerBtn.disabled = false
+    }
+
+    if (points < 10000) {
+        germBtn.disabled = true
+    } else {
+        germBtn.disabled = false
+    }
+
+    if (points < 50000) {
+        doberBtn.disabled = true
+    } else {
+        doberBtn.disabled = false
     }
 }
 
@@ -291,7 +239,12 @@ let bgMusic = new Howl({
     src: ['Caketown 1.mp3'],
     autoplay: true,
     loop: true,
-    volume: 0.3,
+    volume: 0.25,
+})
+
+let doberBark = new Howl({
+    src: ['dog.mp3'],
+    volume: 0.6,
 })
 
 function muteMusic (){
